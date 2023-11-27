3f76ee0f0f7ef1ca69e7f8b251a36cdb3fd4e62b
mughieams <mughieams24@gmail.com>
2023-11-28 01:11:20 +0700

    feat(booking): add jam booking when checking booking

--- src/pages/api/booking/checkbooking.js
+++ src/pages/api/booking/checkbooking.js
@@ -3,19 +3,19 @@ import excuteQuery from "../../../../lib/db";
 import NextCors from 'nextjs-cors';
 
 export default async function bookingCheck(req, res) {
-    const { today, id_dokter} = req.body;
-    
+    const { today, id_dokter, time } = req.body;
+
     await NextCors(req, res, {
         // Options
         methods: ['POST'],
         origin: '*',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
      });
-     
+
     try {
         const result = await excuteQuery({
-            query: "SELECT a.id, a.tanggal_booking, a.jam_booking, a.id_dokter FROM booking a where a.payment_status = 'settlement' and a.tanggal_booking > ? and a.id_dokter = ?",
-            values:[today, id_dokter],
+            query: "SELECT a.id, a.tanggal_booking, a.jam_booking, a.id_dokter FROM booking a where a.payment_status = 'settlement' and a.tanggal_booking > ? and a.id_dokter = ? and a.jam_booking = ?",
+            values:[today, id_dokter, time],
         });
         res.status(200).json({ result })
     } catch (error) {
