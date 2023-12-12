7cd4c78c0837fcff15a6da7280dda5217451f838
mughieams <mughieams24@gmail.com>
2023-12-12 18:44:37 +0700

    add payment status for manual booking

--- src/pages/api/booking/add.js
+++ src/pages/api/booking/add.js
@@ -2,7 +2,7 @@ import excuteQuery from "../../../../lib/db";
 import NextCors from 'nextjs-cors';
 
 export default async function createnewcustomer(req, res) {
-    const { nama, tanggal_booking, jam_booking, kategori, no_rekam_medis, keluhan, id_dokter, phone, action_status } = req.body;
+    const { nama, tanggal_booking, jam_booking, kategori, no_rekam_medis, keluhan, id_dokter, phone, action_status, payment } = req.body;
     // const tgllahir = new Date(tgl_lahir)
 
     await NextCors(req, res, {
@@ -13,9 +13,13 @@ export default async function createnewcustomer(req, res) {
      });
 
     try {
+        var payment_status = ''
+        if (payment == 'manual') {
+            payment_status = 'settlement'
+        }
         const result = await excuteQuery({
-            query: 'INSERT INTO booking (nama, tanggal_booking, jam_booking, kategori, no_rekam_medis, keluhan, id_dokter, phone, action_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            values:[nama, tanggal_booking, jam_booking, kategori, no_rekam_medis, keluhan, id_dokter, phone, action_status],
+            query: 'INSERT INTO booking (nama, tanggal_booking, jam_booking, kategori, no_rekam_medis, keluhan, id_dokter, phone, action_status, payment_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            values:[nama, tanggal_booking, jam_booking, kategori, no_rekam_medis, keluhan, id_dokter, phone, action_status, payment_status],
         });
         res.status(200).json({ result })
     } catch (error) {
